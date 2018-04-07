8abb6420523749db686b6096f942cdaa1e7a699f
Edgar Passos <edgarlpassos@gmail.com>
2018-04-07 20:04:03 +0100

    Fixed routing on nginx. Added React-Router and a test API call

--- client/src/index.js
+++ client/src/index.js
@@ -1,8 +1,13 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import ReactDom from 'react-dom';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+import App from './App.js';
+
+ReactDom.render(
+    <Router>
+        <div>
+            <Route exact path="/" component={App}/>
+        </div>
+    </Router>, document.getElementById('root')
+    );
