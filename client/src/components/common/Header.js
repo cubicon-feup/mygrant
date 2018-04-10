da44bc12a9cfd4e496a49e8b53697d1841a0a5a8
Edgar Passos <edgarlpassos@gmail.com>
2018-04-10 11:28:45 +0100

    Adding Header Component

--- client/src/components/common/Header.js
+++ client/src/components/common/Header.js
@@ -1,16 +1,14 @@
 import React, { Component } from 'react';
-import { Menu } from 'semantic-ui-react';
+import { Menu, Container } from 'semantic-ui-react';
 // import '../../css/common.css';
 
 class Header extends Component {
     render() {
         return (
             <Menu>
-                <Menu.Item
-                    name="Mygrant"
-                >
-                Mygrant
-                </Menu.Item>
+                <Menu.Item header name="mygrant" >Mygrant</Menu.Item>
+                <Menu.Item position="right">Login</Menu.Item>
+                <Menu.Item>Sign Up</Menu.Item>
             </Menu>
         );
     }
