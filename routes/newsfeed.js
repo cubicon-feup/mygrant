4da3501122ba5b0f9cae672bb2796051d8d70fa3
Bruno Santos <bruno.santos1996@gmail.com>
2018-04-13 11:50:29 +0100

    newsfeed routes clean up

--- routes/newsfeed.js
+++ routes/newsfeed.js
@@ -16,13 +16,13 @@ function getQuery(filename) {
 //
 
 // Queries
-var sqlGetNewsfeed = getQuery('getNewsfeed');
-var sqlGetUserfeed = getQuery('getUserfeed')
-var sqlCreatePost = getQuery('createPost');
-var sqlGetPost = getQuery('getPost');
-var sqlEditPost = getQuery('editPost');
-var sqlLikePost = getQuery('likePost');
-var sqlUnlikePost = getQuery('unlikePost');
+var sqlGetNewsfeed = getQuery('newsfeed/getNewsfeed');
+var sqlGetUserfeed = getQuery('newsfeed/getUserfeed')
+var sqlCreatePost = getQuery('newsfeed/createPost');
+var sqlGetPost = getQuery('newsfeed/getPost');
+var sqlEditPost = getQuery('newsfeed/editPost');
+var sqlLikePost = getQuery('newsfeed/likePost');
+var sqlUnlikePost = getQuery('newsfeed/unlikePost');
 
 
 // Get all posts from friends
@@ -95,6 +95,10 @@ router.put('/post/:id/edit', function(req, res) {
 	});
 });
 
+// Delete a post
+router.put('post/:id/delete', function(req, res) {
+});
+
 // Like a post
 router.put('/post/:id/like', function(req, res) {
 	var user_id = 1;
@@ -120,88 +124,6 @@ router.delete('/post/:id/unlike', function(req, res) {
 	});
 	
 });
-
-/*
-// Get list of all services
-router.get('/', function(req, res) {
-  res.json({ teste: 'lista de servicos' });
-});
-
-// Search service by various parameters
-// filters: by name, by date, by distance, by popularity, by user
-router.get('/search', function(req, res) {
-	res.json(req.query); // print out the params
-	// TODO
-});
-
-// Get service by id
-router.get('/:id', function(req, res) {
-	// TODO
-  res.json({ teste: req.params.id });
-});
-
-// Put (create) service.
-router.put('/', function(req, res) {
-	// TODO
-});
-
-// Put (update) service.
-router.put('/:id', function(req, res) {
-	// TODO
-});
-
-// Delete service.
-router.delete('/:id', function(req, res) {
-	// TODO
-});
-
-//
-//
-// IMAGES
-//
-//
-
-// Get images.
-router.get('/:id/images', function(req, res) {
-	// TODO
-});
-
-// Add image.
-router.put('/:id/images', function(req, res) {
-	// TODO
-});
-
-// Delete image.
-router.delete('/:id/images/:image', function(req, res) {
-	// TODO
-});
-
-//
-//
-// OFFERS
-//
-//
-
-// Get list of offers.
-router.get('/:id/offers', function(req, res) {
-	// TODO
-});
-
-// Get specific offer.
-router.get('/:id/offers/:offer', function(req, res) {
-	// TODO
-});
-
-// Pick an offer.
-router.post('/:id/offers/:offer', function(req, res) {
-	// TODO
-});
-
-// Remove offer.
-router.delete('/:id/offers/:offer', function(req, res) {
-	// TODO
-});
-*/
 
 
 module.exports = router;
