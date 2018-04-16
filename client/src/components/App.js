1f1a0d0936ea6989bf376b9e3fe9103cc7f5973e
Edgar Passos <edgarlpassos@gmail.com>
2018-04-16 20:37:50 +0100

    First version of buttons

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Button, Container, Grid, Icon } from 'semantic-ui-react';
+import { Button, Container, Grid, Header, Icon } from 'semantic-ui-react';
 import '../css/App.css';
 
 class App extends Component {
@@ -16,37 +16,35 @@ class App extends Component {
 
   render() {
     return (
-        <Container className="main-container">
-            <Grid columns={2}>
-                <Grid.Row>
-                    <Grid.Column></Grid.Column>
-                    <Grid.Column>
-                        <Button fluid circular className="mygrant-button" >Log In</Button>
-                    </Grid.Column>
-                </Grid.Row>
-                <Grid.Row>
-                    <Grid.Column></Grid.Column>
-                    <Grid.Column>
-                        <Button fluid circular className="mygrant-button">Sign Up</Button>
-                    </Grid.Column>
-                </Grid.Row>
-                <Grid.Row textAlign="center" >
-                    <Grid.Column></Grid.Column>
-                    <Grid.Column>
-                        <Icon name="fas fa-circle" />
-                        <Icon name="fas fa-circle" />
-                        <Icon name="fas fa-circle" />
-                    </Grid.Column>
-                </Grid.Row>
-                <Grid.Row>
-                    <Grid.Column></Grid.Column>
-                    <Grid.Column text-align="center" vertical-align="middle" >
-                        <Button fluid circular color="facebook">
-                            <Icon name="fab fa-facebook" />Log In with Facebook
-                        </Button>
-                    </Grid.Column>
-                </Grid.Row>
-            </Grid>
+        <Container fluid className="main-container">
+            <Container className="landing-page-buttons" >
+                <Grid columns={3} > 
+                    <Grid.Row >
+                        <Grid.Column floated="right" >
+                            <Button fluid circular className="mygrant-button" >Log In</Button>
+                        </Grid.Column>
+                    </Grid.Row>
+                    <Grid.Row>
+                        <Grid.Column floated="right" >
+                            <Button fluid circular className="mygrant-button">Sign Up</Button>
+                        </Grid.Column>
+                    </Grid.Row>
+                    <Grid.Row textAlign="center" >
+                        <Grid.Column floated="right" >
+                            <Icon name="fas fa-circle" />
+                            <Icon name="fas fa-circle" />
+                            <Icon name="fas fa-circle" />
+                        </Grid.Column>
+                    </Grid.Row>
+                    <Grid.Row>
+                        <Grid.Column floated="right" >
+                            <Button fluid circular color="facebook">
+                                <Icon name="fab fa-facebook" />Log In with Facebook
+                            </Button>
+                        </Grid.Column>
+                    </Grid.Row>
+                </Grid>
+            </Container>
         </Container>
     );
   }
