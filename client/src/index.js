663c35240f8e8af667813f06f69f98608507af7e
Diogo Cruz <diogohmcruz@hotmail.com>
2018-04-20 21:44:24 +0100

    Simple User page

--- client/src/index.js
+++ client/src/index.js
@@ -1,17 +1,18 @@
-import React from "react";
-import ReactDom from "react-dom";
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import React from 'react';
+import ReactDom from 'react-dom';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 
-import "semantic-ui-css/semantic.min.css";
-import "./css/common.css";
+import 'semantic-ui-css/semantic.min.css';
+import './css/common.css';
 
-import registerServiceWorker from "./registerServiceWorker";
-import App from "./components/App";
-import Login from "./components/Login";
-import Signup from "./components/Signup";
-import CreateService from "./components/CreateService";
-import { Responsive } from "semantic-ui-react";
-import { MygrantFooter, MygrantHeader, MygrantNav } from "./components/Common";
+import registerServiceWorker from './registerServiceWorker';
+import App from './components/App';
+import Login from './components/Login';
+import Signup from './components/Signup';
+import CreateService from './components/CreateService';
+import User from './components/User';
+import { Responsive } from 'semantic-ui-react';
+import { MygrantFooter, MygrantHeader, MygrantNav } from './components/Common';
 
 ReactDom.render(
     <Router>
@@ -21,11 +22,12 @@ ReactDom.render(
             <Route exact path="/login" component={Login} />
             <Route exact path="/signup" component={Signup} />
             <Route exact path="/createservice" component={CreateService} />
+            <Route exact path="/user/:id" component={User} />
             <Responsive as={MygrantFooter} minWidth={768} />
             <Responsive as={MygrantNav} maxWidth={768} />
         </div>
     </Router>,
-    document.getElementById("root")
+    document.getElementById('root')
 );
 
 registerServiceWorker();
