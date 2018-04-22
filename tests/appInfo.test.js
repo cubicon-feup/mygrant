01157f4bdcaf704d1b853f50b6983a0c1ed366ca
Diogo Cruz <diogohmcruz@hotmail.com>
2018-04-22 11:23:34 +0100

    Simplify root test

--- tests/appInfo.test.js
+++ tests/appInfo.test.js
@@ -2,14 +2,11 @@ const request = require('supertest');
 const app = require('../app');
 
 describe('Test the root path', () => {
-    test('It should response the GET method', done => {
-        request(app)
+    test('It should response the GET method', () => request(app)
             .get('/')
             .then(response => {
                 expect(response.statusCode).toBe(200);
-                done();
-            });
-    });
+            }));
 });
 
 describe('appInfo routes test', function() {
