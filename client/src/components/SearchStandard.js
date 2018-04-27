b3f1c100e26e74f5111e1aebee1043155522a580
dcepa95 <dcepa95@gmail.com>
2018-04-27 17:42:44 +0100

    Intermediate stuff

---
+++ client/src/components/SearchStandard.js
@@ -0,0 +1,144 @@
+import React, { Component } from 'react';
+import '../css/common.css';
+import { Link } from 'react-router-dom';
+import { Container, Header, Loader, Item, Button, Input, Menu, Dropdown, Form, Accordion} from 'semantic-ui-react';
+
+const urlForCrowdfundings = 'http://localhost:3001/api/crowdfundings';
+
+const panels = [
+    {
+        title: 'Optional Details',
+        content: {
+            as: Form.Input,
+            key: 'content',
+            label: 'Maiden Name',
+            placeholder: 'Maiden Name',
+        },
+    },
+]
+
+class SearchStandard extends Component {
+
+    componentDidMount() {
+        fetch(urlForCrowdfundings)
+            .then(response => {
+                if (!response.ok) {
+                    throw Error('Network request failed');
+                }
+
+                return response;
+            })
+            .then(result => result.json())
+            .then(result => {
+                this.setState({ crowdfundings: result });
+            }, () => {
+                // "catch" the error
+                this.setState({ requestFailed: true });
+            });
+    }
+
+    constructor(props) {
+        super(props);
+        this.list = [
+            {
+                'id': 0,
+                'title': 'Project1',
+                'description': 'hueee',
+                'location': 'ali',
+                'earned': '',
+                'target': '',
+                'end_date': ''
+            },
+            {
+                'id': 1,
+                'title': 'Project2',
+                'description': '',
+                'location': '',
+                'earned': '',
+                'target': '',
+                'end_date': ''
+            }
+        ];
+        this.state = {};
+        this.table_body = {};
+    }
+
+    render() {
+        const { activeIndex } = this.state
+        if(!this.state.crowdfundings){
+            return (
+                <Container className="main-container">
+                    <div>
+                        <Loader active inline='centered' />
+                    </div>
+                </Container>
+            );
+        }else{
+            this.table_body = this.state.crowdfundings.map(table_row => {
+                return (
+                    <Item>
+                        <Item.Image size='small' src='/assets/images/wireframe/image.png' />
+
+                        <Item.Content verticalAlign='middle'>
+                            <Item.Header>{table_row.title}</Item.Header>
+                            <Item.Meta><a>{table_row.category}</a> <a>{table_row.creator_name}</a></Item.Meta>
+                            <Item.Description>{table_row.status}</Item.Description>
+                            <Item.Extra>
+                                <Link to={"/crowdfunding/" + table_row.id}><Button  floated="right">See Details</Button></Link>
+                            </Item.Extra>
+                        </Item.Content>
+                    </Item>
+                );
+                /*<Table.Row>
+                        <Table.Cell>{table_row.title}</Table.Cell>
+                        <Table.Cell>{table_row.category}</Table.Cell>
+                        <Table.Cell>{table_row.location}</Table.Cell>
+                        <Table.Cell>{table_row.status}</Table.Cell>
+                        <Table.Cell>{table_row.mygrant_target}</Table.Cell>
+                        <Table.Cell>{table_row.creator_name}</Table.Cell>
+                        <Table.Cell><Link to={"/crowdfunding/" + 2} >Link</Link></Table.Cell>
+                    </Table.Row>*/
+            });
+        }
+        return (
+            <Container className="main-container">
+                <Menu>
+                    <Menu.Item>
+                        <Input action={{ type: 'submit', content: 'Search' }} placeholder='Search' />
+                    </Menu.Item>
+                    <Menu.Item>
+                        <Input placeholder='order dropdwon'/>
+                    </Menu.Item>
+                    <Menu.Item>
+                        <Input placeholder='filter dropdwon'/>
+                    </Menu.Item>
+                    <p>hueeeeee</p>
+                </Menu>
+                <div>
+                    <Header as="h1">Crowdfundings</Header>
+                    <Item.Group divided>
+                        {this.table_body}
+                    </Item.Group>
+                </div>
+            </Container>
+        );
+        /*<Table selectable>
+                        <Table.Header>
+                            <Table.Row>
+                                <Table.HeaderCell>Title</Table.HeaderCell>
+                                <Table.HeaderCell>Category</Table.HeaderCell>
+                                <Table.HeaderCell>Location</Table.HeaderCell>
+                                <Table.HeaderCell>Status</Table.HeaderCell>
+                                <Table.HeaderCell>Target</Table.HeaderCell>
+                                <Table.HeaderCell>Owner</Table.HeaderCell>
+                                <Table.HeaderCell>Link</Table.HeaderCell>
+                            </Table.Row>
+                        </Table.Header>
+                        <Table.Body>
+                            {this.table_body}
+                        </Table.Body>
+                    </Table>*/
+    }
+}
+
+export default SearchStandard;
--- client/src/index.js
+++ client/src/index.js
@@ -10,6 +10,10 @@ import App from "./components/App";
 import Login from "./components/Login";
 import Signup from "./components/Signup";
 import CreateService from "./components/CreateService";
+import Search from "./components/SearchStandard";
+import CreateCrowdfunding from "./components/CreateCrowdfunding";
+import Crowdfunding from "./components/Crowdfunding";
+import Crowdfundings from "./components/Crowdfundings";
 import { Responsive } from "semantic-ui-react";
 import { MygrantFooter, MygrantHeader, MygrantNav } from "./components/Common";
 
@@ -21,6 +25,10 @@ ReactDom.render(
             <Route exact path="/login" component={Login} />
             <Route exact path="/signup" component={Signup} />
             <Route exact path="/createservice" component={CreateService} />
+            <Route exact path="/search" component={Search} />
+            <Route exact path="/crowdfundings" component={Crowdfundings} />
+            <Route exact path="/crowdfunding/:id" component={Crowdfunding} />
+            <Route exact path="/createcrowdfunding" component={CreateCrowdfunding} />
             <Responsive as={MygrantFooter} minWidth={768} />
             <Responsive as={MygrantNav} maxWidth={768} />
         </div>
