ae6493422f2f5b1789d1c8d1c01ffc3d293dde0e
Evenilink <evenilink.code@gmail.com>
2018-04-27 18:51:36 +0100

    Fixed minor bug.

--- policies/crowdfundingsPolicy.js
+++ policies/crowdfundingsPolicy.js
@@ -13,15 +13,15 @@ module.exports = {
             location: Joi.string().regex(
                 new RegExp(config.regex.line)
             ),
-            mygrantTarget: Joi.number().required(),
-            dateFinished: Joi.date().required(),
+            mygrant_target: Joi.number().required(),
+            // dateFinished: Joi.date().required(),
             creator_id: Joi.number().required()
         }
 
         const {error} = Joi.validate(req.body, schema, config.joiOptions);
 
         if(error) {
-            res.status(400).send({error: 'Invalid public space data.'});
+            res.status(400).send({error: 'Invalid mission data.'});
             console.log(error);
         }
         else next();
