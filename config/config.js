f6568eed412706711afe747255eb2d6f718c91a2
Evenilink <evenilink.code@gmail.com>
2018-04-29 23:47:44 +0100

    Implemented email notification when sending a message. All that's left is getting the user configurations to know if he wants to be notified, and also send the message URL in the notification email.

--- config/config.js
+++ config/config.js
@@ -9,5 +9,16 @@ module.exports = {
         password: /^[a-zA-Z0-9]{8,32}$/,
         email: /^.*@.*$/,
         name: /^[a-zA-Z0-9 záàâãéèêíïóôõöúçñÁÀÂÃÉÈÍÏÓÔÕÖÚÇÑ.,?!\-;:()]{3,32}$/
+    },
+    transporterOptions: {
+        service: 'Gmail',
+        auth: {
+            user: process.env.MYGRANT_EMAIL,
+            pass: process.env.MYGRANT_PASS
+        }
+    },
+    messageNotificationOptions: {
+        from: `"Mygrant - New Message" <${process.env.MYGRANT_EMAIL}>`, // sender address
+        subject: 'Message Notification', // Subject line
     }
 }
