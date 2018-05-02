b65003f117204b3440becae69157918b8582cc79
Edgar Passos <epassos26@gmail.com>
2018-05-02 19:38:01 +0100

    Fix/environment (#35)
    * Added migrations
    
    * Commented unsupported step

--- migrations/1-2.sql
+++ migrations/1-2.sql
@@ -1,7 +1,7 @@
 -- DATABASE SETTINGS
 SET statement_timeout = 0;
 SET lock_timeout = 0;
-SET idle_in_transaction_session_timeout = 0;
+--SET idle_in_transaction_session_timeout = 0;
 SET client_encoding = 'UTF8';
 SET standard_conforming_strings = on;
 SELECT pg_catalog.set_config('search_path', 'public', false);
