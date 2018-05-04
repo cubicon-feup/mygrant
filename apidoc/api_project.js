590588bbf1d0d0d058d2decb6f7326626fbba662
Bruno Santos <bruno.santos1996@gmail.com>
2018-05-04 17:02:56 +0100

    merge fix

--- apidoc/api_project.js
+++ apidoc/api_project.js
@@ -7,11 +7,7 @@ define({
   "apidoc": "0.3.0",
   "generator": {
     "name": "apidoc",
-<<<<<<< HEAD
     "time": "2018-04-30T14:47:35.659Z",
-=======
-    "time": "2018-04-28T19:17:37.842Z",
->>>>>>> develop
     "url": "http://apidocjs.com",
     "version": "0.17.6"
   }
