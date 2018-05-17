92264a67030f21c4154ffec6b8d6d485d283c678
Edgar Passos <edgarlpassos@gmail.com>
2018-05-17 17:05:14 +0100

    Responsiveness on blog header post

--- client/src/components/BlogHeaderPost.js
+++ client/src/components/BlogHeaderPost.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
-import { Container, Grid, Header, Icon, Image, Segment } from 'semantic-ui-react';
+import { Container, Grid, Header, Icon, Image, Responsive } from 'semantic-ui-react';
 import { instanceOf, PropTypes } from 'prop-types';
 import moment from 'moment';
 
@@ -18,48 +17,59 @@ class BlogHeaderPost extends Component {
     render () {
         return (
             <div>
-                    <Container >
-                        <Grid padded >
-                            <Grid.Row>
-                                <Grid.Column width={16}>
-                                    <Grid>
-                                        <Grid.Row>
-                                            <Grid.Column width={2}>
-                                                <Image circular src={`/api/images/${this.props.user.pictureUrl}`}/>
-                                            </Grid.Column>
-                                            <Grid.Column width={14}>
-                                                <Header as={'h3'}>
-                                                    {this.props.user.fullName}
-                                                </Header>
-                                                <Header.Subheader verticalAlign={'top'}>
-                                                    {this.props.postInfo.datePosted}
-                                                </Header.Subheader>
-                                            </Grid.Column>
-                                        </Grid.Row>
-                                        <Grid.Row className={'content'} verticalAlign={'top'} >
-                                            <Grid.Column width={16} >
-                                                <h3>{this.props.postInfo.content}</h3>
-                                            </Grid.Column>
-                                        </Grid.Row>
-                                        <Grid.Row textAlign={'left'}>
-                                            <Grid.Column width={2}>
-                                                <Icon name={'comment outline'}/>{this.props.postInfo.commentCount}
-                                            </Grid.Column>
-                                            <Grid.Column width={2}>
-                                                <span className={'post-likes'}>
-                                                    <Icon className={'post-likes-icon'} name={'like outline'}/>
-                                                    {this.props.postInfo.likes}
-                                                </span>
-                                            </Grid.Column>
-                                            <Grid.Column width={2}>
-                                                <Icon className={'post-options'} name={'ellipsis horizontal'}/>
-                                            </Grid.Column>
-                                        </Grid.Row>
-                                    </Grid>
-                                </Grid.Column>
-                            </Grid.Row>
-                        </Grid>
-                    </Container>
+                <Container >
+                    <Grid padded >
+                        <Grid.Row>
+                            <Grid.Column width={16}>
+                                <Grid>
+                                    <Grid.Row>
+                                        <Responsive as={Grid.Column} width={2} minWidth={768} >
+                                            <Image circular src={`/api/images/${this.props.user.pictureUrl}`}/>
+                                        </Responsive>
+                                        <Responsive as={Grid.Column} width={14} minWidth={768} >
+                                            <Header as={'h3'}>
+                                                {this.props.user.fullName}
+                                            </Header>
+                                            <Header.Subheader verticalAlign={'top'}>
+                                                {this.props.postInfo.datePosted}
+                                            </Header.Subheader>
+                                        </Responsive>
+                                        <Responsive as={Grid.Column} width={4} maxWidth={768} >
+                                            <Image circular src={`/api/images/${this.props.user.pictureUrl}`}/>
+                                        </Responsive>
+                                        <Responsive as={Grid.Column} width={12} maxWidth={768} >
+                                            <Header as={'h3'}>
+                                                {this.props.user.fullName}
+                                            </Header>
+                                            <Header.Subheader verticalAlign={'top'}>
+                                                {this.props.postInfo.datePosted}
+                                            </Header.Subheader>
+                                        </Responsive>
+                                    </Grid.Row>
+                                    <Grid.Row className={'content'} verticalAlign={'top'} >
+                                        <Grid.Column width={16} >
+                                            <h3>{this.props.postInfo.content}</h3>
+                                        </Grid.Column>
+                                    </Grid.Row>
+                                    <Grid.Row textAlign={'left'}>
+                                        <Grid.Column width={2}>
+                                            <Icon name={'comment outline'}/>{this.props.postInfo.commentCount}
+                                        </Grid.Column>
+                                        <Grid.Column width={2}>
+                                            <span className={'post-likes'}>
+                                                <Icon className={'post-likes-icon'} name={'like outline'}/>
+                                                {this.props.postInfo.likes}
+                                            </span>
+                                        </Grid.Column>
+                                        <Grid.Column width={2}>
+                                            <Icon className={'post-options'} name={'ellipsis horizontal'}/>
+                                        </Grid.Column>
+                                    </Grid.Row>
+                                </Grid>
+                            </Grid.Column>
+                        </Grid.Row>
+                    </Grid>
+                </Container>
             </div>
         );
     }
