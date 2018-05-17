88e9afc76677d7767368dedd6540eb28d7a6cb5e
Diogo Cruz <diogohmcruz@hotmail.com>
2018-05-17 20:19:57 +0100

    Reply to Comment

--- client/src/components/Comments.js
+++ client/src/components/Comments.js
@@ -18,7 +18,8 @@ class CommentsSection extends Component {
             type: this.props.type,
             id: this.props.id,
             comments: [{}],
-            showComments: false
+            showComments: false,
+            in_reply_to: ''
         };
     }
 
@@ -44,7 +45,10 @@ class CommentsSection extends Component {
             );
     }
 
-    // TODO: in_reply_to
+    handleReplyClick = comment_id => e => {
+        this.setState({ in_reply_to: comment_id });
+    };
+
     handleSubmit = e => {
         const { cookies } = this.props;
         e.preventDefault();
@@ -53,7 +57,7 @@ class CommentsSection extends Component {
             method: 'POST',
             body: JSON.stringify({
                 message: this.state.message,
-                in_reply_to: null
+                in_reply_to: this.state.in_reply_to
             }),
             headers: {
                 Authorization: `Bearer ${cookies.get('id_token')}`,
@@ -75,12 +79,12 @@ class CommentsSection extends Component {
 
     renderComments() {
         return (
-            <Comment.Group minimal id="commentssection">
+            <Comment.Group id="commentssection">
                 <Header as="h4" id="whitetext">
                     Comments
                 </Header>
-                {this.state.comments.map((comment, index) =>
-                    <Comment key={index}>
+                {this.state.comments.map(comment =>
+                    <Comment key={`${comment.comment_id}`}>
                         <Comment.Avatar
                             as={Link}
                             to={urlToUser(comment.sender_id)}
@@ -97,15 +101,42 @@ class CommentsSection extends Component {
                             </Comment.Metadata>
                             <Comment.Text>{comment.message}</Comment.Text>
                         </Comment.Content>
+                        <Comment.Actions>
+                            <Comment.Action
+                                active={
+                                    this.state.in_reply_to ===
+                                    comment.comment_id
+                                }
+                                id="whitetext"
+                                content={'Reply'}
+                                onClick={this.handleReplyClick(
+                                    comment.comment_id
+                                )}
+                            />
+                        </Comment.Actions>
+                        {this.state.in_reply_to === comment.comment_id &&
+                            <Form reply onSubmit={this.handleSubmit}>
+                                <Form.TextArea
+                                    required
+                                    name="message"
+                                    onChange={this.handleChange}
+                                />
+                                <Form.Button
+                                    content={`Reply to ${comment.sender_name}`}
+                                    labelPosition="left"
+                                    icon="edit"
+                                />
+                            </Form>
+                        }
                     </Comment>
                 )}
 
-                <Form reply onSubmit={this.handleSubmit}>
+                <Form onSubmit={this.handleSubmit}>
                     <Form.TextArea
                         required
                         name="message"
-                        value={this.state.message}
                         onChange={this.handleChange}
+                        onClick={this.handleReplyClick(null)}
                     />
                     <Form.Button
                         content="Add Comment"
