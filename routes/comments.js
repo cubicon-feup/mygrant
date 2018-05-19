0a52cbbf727b8153fab196dee897f865d42bd2c2
Diogo Cruz <diogohmcruz@hotmail.com>
2018-05-19 12:57:50 +0100

    Comments send User image

--- routes/comments.js
+++ routes/comments.js
@@ -24,75 +24,103 @@ const authenticate = expressJwt({ secret: appSecret });
 // FIXME: not working with POST method, had to use PUT.
 // TODO: policy.
 router.put('/', authenticate, function(req, res) {
-    let senderId = req.user.id;
-    let message = req.body.message;
+    const senderId = req.user.id;
+    const message = req.body.message;
 
-    let crowdfundingId = req.body.hasOwnProperty('crowdfunding_id') ? req.body.crowdfunding_id : null;
-    let serviceId = req.body.hasOwnProperty('service_id') ? req.body.service_id : null;
+    const crowdfundingId = req.body.hasOwnProperty('crowdfunding_id')
+        ? req.body.crowdfunding_id
+        : null;
+    const serviceId = req.body.hasOwnProperty('service_id')
+        ? req.body.service_id
+        : null;
     let serviceOrCrowdfundingId;
     let serviceOrCrowdfunding;
-    if((crowdfundingId != null && serviceId != null) || (crowdfundingId == null && serviceId == null)) {
-        res.status(400).json({error: 'Can only insert a comment either on a service or a crowdfunding.'})
+    if (
+        crowdfundingId != null && serviceId != null ||
+        crowdfundingId == null && serviceId == null
+    ) {
+        res.status(400).json({
+            error:
+                'Can only insert a comment either on a service or a crowdfunding.'
+        });
+
         return;
     } else if (crowdfundingId != null) {
         serviceOrCrowdfunding = 'crowdfunding_id';
-        serviceOrCrowdfundingId = crowdfundingId
+        serviceOrCrowdfundingId = crowdfundingId;
     } else {
         serviceOrCrowdfunding = 'service_id';
         serviceOrCrowdfundingId = serviceId;
     }
 
-    let inReplyTo = req.body.hasOwnProperty('in_reply_to') ? req.body.in_reply_to : null;
-    let query =
-        `INSERT INTO comment (sender_id, message, ${serviceOrCrowdfunding}, date_posted, in_reply_to)
+    const inReplyTo = req.body.hasOwnProperty('in_reply_to')
+        ? req.body.in_reply_to
+        : null;
+    const query = `
+        INSERT INTO comment (sender_id, message, ${serviceOrCrowdfunding}, date_posted, in_reply_to)
         VALUES ($(sender_id), $(message), $(service_or_crowdfunding_id), NOW(), $(in_reply_to))
         RETURNING id, date_posted;`;
 
-    db.one(query, {
-        sender_id: senderId,
-        message: message,
-        service_or_crowdfunding_id: serviceOrCrowdfundingId,
-        in_reply_to: inReplyTo
-    }).then(data => {
-        res.status(201).json(data);
-    }).catch(error => {
-        res.status(500).json(error);
-    })
+    db
+        .one(query, {
+            sender_id: senderId,
+            message,
+            service_or_crowdfunding_id: serviceOrCrowdfundingId,
+            in_reply_to: inReplyTo
+        })
+        .then(data => {
+            res.status(201).json(data);
+        })
+        .catch(error => {
+            res.status(500).json(error);
+        });
 });
 
 router.get('/', function(req, res) {
-    let crowdfundingId = req.query.hasOwnProperty('crowdfunding_id') ? req.query.crowdfunding_id : null;
-    let serviceId = req.query.hasOwnProperty('service_id') ? req.query.service_id : null;
+    const crowdfundingId = req.query.hasOwnProperty('crowdfunding_id')
+        ? req.query.crowdfunding_id
+        : null;
+    const serviceId = req.query.hasOwnProperty('service_id')
+        ? req.query.service_id
+        : null;
     let serviceOrCrowdfundingId;
     let serviceOrCrowdfunding;
-    if((crowdfundingId != null && serviceId != null) || (crowdfundingId == null && serviceId == null)) {
-        res.status(400).json({error: 'Can only insert a comment either on a service or a crowdfunding.'})
+    if (
+        crowdfundingId != null && serviceId != null ||
+        crowdfundingId == null && serviceId == null
+    ) {
+        res.status(400).json({
+            error:
+                'Can only insert a comment either on a service or a crowdfunding.'
+        });
+
         return;
     } else if (crowdfundingId != null) {
         serviceOrCrowdfunding = 'crowdfunding_id';
-        serviceOrCrowdfundingId = crowdfundingId
+        serviceOrCrowdfundingId = crowdfundingId;
     } else {
         serviceOrCrowdfunding = 'service_id';
         serviceOrCrowdfundingId = serviceId;
     }
 
-    let query =
-        `SELECT comment.sender_id as user_id, users.full_name as user_name, comment.id as comment_id, comment.message, date_posted, in_reply_to
+    const query = `
+        SELECT comment.sender_id as user_id, users.full_name as user_name, users.image_url as image_url, comment.id as comment_id, comment.message, date_posted, in_reply_to
         FROM comment
         INNER JOIN users ON users.id = comment.sender_id
         WHERE comment.${serviceOrCrowdfunding} = $(service_or_crowdfunding_id)
         ORDER BY date_posted;`;
 
-    db.manyOrNone(query, {
-        service_or_crowdfunding_id: serviceOrCrowdfundingId
-    }).then(data => {
-        res.status(200).json(data);
-    }).catch(error => {
-        res.status(500).json({error});
-    })
-})
+    db
+        .manyOrNone(query, { service_or_crowdfunding_id: serviceOrCrowdfundingId })
+        .then(data => {
+            res.status(200).json(data);
+        })
+        .catch(error => {
+            res.status(500).json({ error });
+        });
+});
 
- /**
+/**
  * @api {get} /comments/top_comments Get top comments (those that are not a reply)
  * @apiName GetTopComments
  * @apiGroup CommentStandard
@@ -111,37 +139,49 @@ router.get('/', function(req, res) {
  */
 // TODO: policy.
 router.get('/top_comments', function(req, res) {
-    let crowdfundingId = req.query.hasOwnProperty('crowdfunding_id') ? req.query.crowdfunding_id : null;
-    let serviceId = req.query.hasOwnProperty('service_id') ? req.query.service_id : null;
+    const crowdfundingId = req.query.hasOwnProperty('crowdfunding_id')
+        ? req.query.crowdfunding_id
+        : null;
+    const serviceId = req.query.hasOwnProperty('service_id')
+        ? req.query.service_id
+        : null;
     let serviceOrCrowdfundingId;
     let serviceOrCrowdfunding;
-    if((crowdfundingId != null && serviceId != null) || (crowdfundingId == null && serviceId == null)) {
-        res.status(400).json({error: 'Can only insert a comment either on a service or a crowdfunding.'})
+    if (
+        crowdfundingId != null && serviceId != null ||
+        crowdfundingId == null && serviceId == null
+    ) {
+        res.status(400).json({
+            error:
+                'Can only insert a comment either on a service or a crowdfunding.'
+        });
+
         return;
     } else if (crowdfundingId != null) {
         serviceOrCrowdfunding = 'crowdfunding_id';
-        serviceOrCrowdfundingId = crowdfundingId
+        serviceOrCrowdfundingId = crowdfundingId;
     } else {
         serviceOrCrowdfunding = 'service_id';
         serviceOrCrowdfundingId = serviceId;
     }
 
-    let query =
-        `SELECT comment.sender_id as user_id, users.full_name as user_name, comment.id as comment_id, comment.message, date_posted, in_reply_to
+    const query = `
+        SELECT comment.sender_id as user_id, users.full_name as user_name, users.image_url as image_url, comment.id as comment_id, comment.message, date_posted, in_reply_to
         FROM comment
         INNER JOIN users ON users.id = comment.sender_id
         WHERE comment.${serviceOrCrowdfunding} = $(service_or_crowdfunding_id)
             AND comment.in_reply_to IS NULL
         ORDER BY date_posted;`;
 
-    db.manyOrNone(query, {
-        service_or_crowdfunding_id: serviceOrCrowdfundingId
-    }).then(data => {
-        res.status(200).json(data);
-    }).catch(error => {
-        res.status(500).json({error});
-    })
-})
+    db
+        .manyOrNone(query, { service_or_crowdfunding_id: serviceOrCrowdfundingId })
+        .then(data => {
+            res.status(200).json(data);
+        })
+        .catch(error => {
+            res.status(500).json({ error });
+        });
+});
 
 /**
  * @api {get} /comments/:comment_id/nested_comments Get nested comments
@@ -157,26 +197,27 @@ router.get('/top_comments', function(req, res) {
  * @apiSuccess (Success 200) {Integer} comment_message Message that the comment holds.
  * @apiSuccess (Success 200) {Integer} date_posted Date the coment was posted.
  * @apiSuccess (Success 200) {Integer} in_reply_to Comment id that the comment is replying (can be null).
- * 
+ *
  * @apiError (Error 500) InternalServerError
  */
 router.get('/:comment_id/nested_comments', function(req, res) {
-    let commentId = req.params.comment_id;
-    let query =
-        `SELECT comment.sender_id as user_id, users.full_name as user_name, comment.id as comment_id, comment.message, date_posted, in_reply_to
+    const commentId = req.params.comment_id;
+    const query = `
+        SELECT comment.sender_id as user_id, users.full_name as user_name, users.image_url as image_url, comment.id as comment_id, comment.message, date_posted, in_reply_to
         FROM comment
         INNER JOIN users ON users.id = comment.sender_id
         WHERE comment.in_reply_to = $(comment_id)
         ORDER BY date_posted;`;
 
-    db.manyOrNone(query, {
-        comment_id: commentId
-    }).then(data => {
-        res.status(200).json(data);
-    }).catch(error => {
-        res.status(500).json({error});
-    })
-})
+    db
+        .manyOrNone(query, { comment_id: commentId })
+        .then(data => {
+            res.status(200).json(data);
+        })
+        .catch(error => {
+            res.status(500).json({ error });
+        });
+});
 
 /**
  * @api {put} /comments/:comment_id Update comment
@@ -193,24 +234,27 @@ router.get('/:comment_id/nested_comments', function(req, res) {
  */
 // TODO: policy.
 router.put('/:comment_id', authenticate, function(req, res) {
-    let senderId = req.user.id;
-    let commentId = req.params.comment_id;
-    let message = req.body.message;
-    let query =
-        `UPDATE comment
+    const senderId = req.user.id;
+    const commentId = req.params.comment_id;
+    const message = req.body.message;
+    const query = `
+        UPDATE comment
         SET message = $(message)
         WHERE id = $(comment_id)
             AND sender_id = $(sender_id);`;
 
-    db.none(query, {
-        message: message,
-        comment_id: commentId,
-        sender_id: senderId
-    }).then(() => {
-        res.sendStatus(200);
-    }).catch(error => {
-        res.status(500).json({error});
-    })
+    db
+        .none(query, {
+            message,
+            comment_id: commentId,
+            sender_id: senderId
+        })
+        .then(() => {
+            res.sendStatus(200);
+        })
+        .catch(error => {
+            res.status(500).json({ error });
+        });
 });
 
 /**
@@ -227,21 +271,23 @@ router.put('/:comment_id', authenticate, function(req, res) {
  */
 // TODO: policy.
 router.delete('/:comment_id', authenticate, function(req, res) {
-    let senderId = req.user.id;
-    let commentId = req.params.comment_id;
-    let query =
-        `DELETE FROM comment
+    const senderId = req.user.id;
+    const commentId = req.params.comment_id;
+    const query = `DELETE FROM comment
         WHERE id = $(comment_id)
             AND sender_id = $(sender_id);`;
 
-    db.none(query, {
-        comment_id: commentId,
-        sender_id: senderId
-    }).then(() => {
-        res.sendStatus(200);
-    }).catch(error => {
-        res.status(500).json(error);
-    })
+    db
+        .none(query, {
+            comment_id: commentId,
+            sender_id: senderId
+        })
+        .then(() => {
+            res.sendStatus(200);
+        })
+        .catch(error => {
+            res.status(500).json(error);
+        });
 });
 
 module.exports = router;
