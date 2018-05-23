d25e9f24404628d13dff330b23d8331fd55b8dcc
Diogo Cruz <diogohmcruz@hotmail.com>
2018-05-23 17:06:18 +0100

    Responsive Map size
    O mapa do PidgeonMaps precisa de receber um width e um height.
    Para calcular os tamanhos do parent desse component é preciso o pacote react-dimensions.

--- client/src/components/Map.js
+++ client/src/components/Map.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import Map from 'pigeon-maps';
 import Marker from 'pigeon-marker/react';
+import Dimensions from 'react-dimensions';
 
 const getProvider = (x, y, z) =>
     `https://cartodb-basemaps-a.global.ssl.fastly.net/light_all/${z}/${x}/${y}.png`;
@@ -17,6 +18,25 @@ class PigeonMaps extends Component {
         };
     }
 
+    componentDidMount() {
+        var height = this.calculateHeight();
+
+        this.setState({
+            containerWidth: this.props.containerWidth,
+            containerHeight: height
+        });
+    }
+
+    calculateHeight() {
+        if (this.props.containerWidth <= 300) {
+            return 200;
+        } else if (this.props.containerWidth <= 350) {
+            return 300;
+        }
+
+        return 400;
+    }
+
     handleClick = ({ event, latLng }) => {
         this.setState({ latlng: latLng });
         this.props.handleChange && this.props.handleChange(latLng);
@@ -25,8 +45,8 @@ class PigeonMaps extends Component {
     render() {
         return (
             <Map
-                width={900}
-                height={600}
+                width={this.state.containerWidth}
+                height={this.state.containerHeight}
                 defaultCenter={this.state.latlng}
                 defaultZoom={this.state.zoom}
                 provider={getProvider}
@@ -38,4 +58,4 @@ class PigeonMaps extends Component {
     }
 }
 
-export default PigeonMaps;
+export default Dimensions()(PigeonMaps);
