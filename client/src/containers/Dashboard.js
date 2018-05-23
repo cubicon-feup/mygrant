70a16dd9a5937fcfd72b02d6c5c631625e08ef13
Evenilink <evenilink.code@gmail.com>
2018-05-23 16:02:19 +0100

    Removed unecessary code.

--- client/src/containers/Dashboard.js
+++ client/src/containers/Dashboard.js
@@ -6,10 +6,7 @@ import { Container, Header, Grid, Divider, Image, Icon, Item, Rating, Loader,Pro
 import Crowdfunding from '../components/dashboard/Crowdfunding';
 
 const urlGetMygrantBalance = `/api/comments/mygrant_balance`;
-// const urlGetCandidates = serviceId => `/api/services/${serviceId}/offers`;
 const urlGetUserCrowdfundings = `/api/comments/crowdfundings`;
-const urlGetCrowdfundingServices = crowdfundingId => `/api/crowdfundings/${crowdfundingId}/services_requested`;
-const urlGetServiceCandidates = serviceId => `/api/services/${serviceId}/offers`;
 
 class Dashboard extends Component {
     static propTypes = { cookies: instanceOf(Cookies).isRequired };
@@ -49,9 +46,6 @@ class Dashboard extends Component {
         })
     }
 
-    // Get crowdfundings, services and candidates.
-    // STARTING HIERARCHICAL FETCHING!
-
     getUserCrowdfundings() {
         const { cookies } = this.props;
         fetch(urlGetUserCrowdfundings, {
@@ -64,81 +58,10 @@ class Dashboard extends Component {
                 res.json()
                     .then(data => {
                         this.setState({crowdfundings: data.data})
-                        for(let i = 0; i < this.state.crowdfundings.length; i++) {
-                            this.getCrowdfundingServices(this.state.crowdfundings[i].id);
-                        }
                     })
             }
         })
     }
-
-    getCrowdfundingServices(crowdfundingId) {
-        const { cookies } = this.props;
-        fetch(urlGetCrowdfundingServices(crowdfundingId), {
-            method: 'GET',
-            headers: {
-                Authorization: `Bearer ${cookies.get('id_token')}`
-            }
-        }).then(res => {
-            if(res.status === 200) {
-                res.json()
-                    .then(data => {
-                        let crowdfundingsServices = this.state.crowdfundingsServices;
-                        for(let i = 0; i < data.length; i++) {
-                            let service = data[i];
-                            service.crowdfunding_id = crowdfundingId;
-                            crowdfundingsServices.push(service);
-                            this.getServiceCandidates(data[i].id);
-                        }
-                        this.setState({crowdfundingsServices: crowdfundingsServices});
-                    })
-            }
-        })
-    }
-
-    getServiceCandidates(serviceId) {
-        const { cookies } = this.props;
-        fetch(urlGetServiceCandidates(serviceId), {
-            method: 'GET',
-            headers: {
-                Authorization: `Bearer ${cookies.get('id_token')}`
-            }
-        }).then(res => {
-            if(res.status === 200) {
-                res.json()
-                    .then(data => {
-                        let candidates = this.state.candidates;
-                        for(let i = 0; i < data.length; i++) {
-                            let candidate = data[i];
-                            candidate.service_id = serviceId;
-                            candidates.push(candidate);
-                        }
-                        this.setState({candidates: candidates});
-                        // console.log(this.state)
-                    })
-            }
-        })
-    }
-
-    // FINISHED HIERARCHICAL FETCHING.
-
-    // FIXME: not using, can remove.
-    /*getCrowdfundingsAndServices() {
-        const { cookies } = this.props;
-        fetch(urlGetCrowdfundingsAndServices, {
-            method: 'GET',
-            headers: {
-                Authorization: `Bearer ${cookies.get('id_token')}`
-            }
-        }).then(res => {
-            if(res.status === 200) {
-                res.json()
-                    .then(data => {
-                        
-                    })
-            }
-        })
-    }*/
 
     // Crowdfundings that the user supported.
     getCrowdfundingsToRate() {
@@ -155,39 +78,8 @@ class Dashboard extends Component {
 
     }
 
-    /*getCandidatesToCrowdfundingServices() {
-        const { cookies } = this.props;
-        fetch(urlGetCandidates(1003), {
-            method: 'GET',
-            headers: {
-                Authorization: `Bearer ${cookies.get('id_token')}`
-            }
-        }).then(res => {
-            if(res.status === 200) {
-                res.json()
-                    .then(data => {
-                        console.log(data);
-                        this.setState({candidates: data});
-                    })
-            }
-        })
-    }*/
-
     getCandidatesToServices() {
 
-    }
-
-    a() {
-        let crowdfundings;
-        for(let i = 0; i < this.state.crowdfundings.length; i++) {
-            let crowdfunding = 
-                <Container>
-                    <p>{this.state.crowdfundings[i].title}</p>
-                </Container>
-            for(let j = 0; j < this.state.crowdfundingsServices.length; j++) {
-
-            }
-        }
     }
 
     render() {
