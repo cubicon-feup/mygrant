e0924fd5176c3f67e1557a01b8281daf1e9ae872
Diogo Cruz <diogohmcruz@hotmail.com>
2018-05-24 23:10:16 +0100

    PigeonMaps & Nominatim (#56)
    * Install PigeonMaps
    
    * Maps send value to Parent
    
    Se for preciso guardar os valores de latitude e longitude do mapa é preciso enviar um função de handleChange.
    Esta vai retornar um array [latitude, longitude].
    
    * Responsive Map size
    
    O mapa do PidgeonMaps precisa de receber um width e um height.
    Para calcular os tamanhos do parent desse component é preciso o pacote react-dimensions.
    
    * Maps on create crowdfunding
    
    * Maps on Crowdfunding & Service
    
    * Dynamic maps of Crowdfunding & Service
    
    * Cleaner Map code onmount
    
    * Creates have a modal to show map
    
    * Resolve conflits
    
    * SearchLocation
    
    Pesquisa por uma localização em texto.
    
    * SearchLocation receive handlerChange
    
    O handlerChange serve para receber a informação que foi escolhida no SearchLocation.
    
    * Fix modal position
    
    * Better display of searchbox
    
    * Remove SignupInfo page
    
    1. Removo a página SignupInfo;
    2. Removo route /api/users/set_location`;
    3. Route `/api/auth/signup` recebe latitude e longitude;
    
    * Placeholder
    
    * Uniformizar com o resto do signup
    
    * Fix repeat result bug
    
    Se fossem retornados 2 resultados com o mesmo nome, 1 deles não seria apagado ao mudar a procura.

--- client/src/components/Crowdfunding.js
+++ client/src/components/Crowdfunding.js
@@ -9,6 +9,7 @@ import { withCookies, Cookies } from 'react-cookie';
 import CrowdfundingOffers from './service_offers/CrowdfundingOffers';
 import Donator from './Donator';
 import CommentsSection from './Comments';
+import PidgeonMaps from './Map';
 
 const apiPath = require('../config').apiPath;
 const urlForData = crowdfundingId => `/api/crowdfundings/` + crowdfundingId;
@@ -171,7 +172,20 @@ class Crowdfunding extends Component {
         })
     }
 
-  render() {
+    renderMap() {
+        if (this.state.crowdfunding.latitude && this.state.crowdfunding.longitude) {
+            return (
+                <PidgeonMaps
+                    latlng={[
+                        this.state.crowdfunding.latitude,
+                        this.state.crowdfunding.longitude
+                    ]}
+                />
+            );
+        }
+    }
+
+    render() {
 
       if(this.state.requestFailed) {
         return (
@@ -400,6 +414,7 @@ class Crowdfunding extends Component {
                     </Grid.Column>
                 </Grid>
             </Container>
+            {this.renderMap()}
             <CommentsSection type="crowdfundings" id={this.state.crowdfundingId} />
             {/*<Container>
                 <h3>Comments</h3>
