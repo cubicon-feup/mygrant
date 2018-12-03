70a238d2017f1c5210d60f4944bf2cf808100831
Fabio Lino <fabiotmw@msn.com>
2018-12-03 22:51:25 +0000

    merge conflits resolved closes #8 relates to aurman -S google-chrome sudo apcman -S ttf-roboto

--- apidoc/api_project.js
+++ apidoc/api_project.js
@@ -7,8 +7,8 @@ define({
   "apidoc": "0.3.0",
   "generator": {
     "name": "apidoc",
-    "time": "2018-11-06T00:53:05.271Z",
+    "time": "2018-12-03T22:46:33.481Z",
     "url": "http://apidocjs.com",
-    "version": "0.17.6"
+    "version": "0.17.7"
   }
 });
